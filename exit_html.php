1f7e847b05628a929f598fe0482ea97f00ab94e3
Gabriel Anaya Delgado <massif.jpg@gmail.com>
2025-03-08 04:30:05 -0600

    [EXERCISE-01] https://www.php.net/manual/es/language.basic-syntax.phpmode.php

---
+++ exit_html.php
@@ -0,0 +1,3 @@
+<p>This will be ignored by PHP and displayed by the browser.</p>
+<?php echo 'While this is to be interpreted.'; ?>
+<p>This will also be ignored by PHP and displayed by the browser.</p>
